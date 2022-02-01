2a2ee59894494949d68e5b01e5feda502dd7dca3
giannis-rs <98825860+giannis-rs@users.noreply.github.com>
2022-02-01 16:30:43 +0200

    v3

---
+++ lessons/script.js
@@ -0,0 +1,21 @@
+function drop(id) {
+    let x = document.getElementById(id);
+    x.classList.toggle("w3-show");
+
+    /*
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += "w3-show";
+        console.log(x.className);
+        console.log("true");
+        return;
+    } else {
+        //x.classname = x.className.replace("w3-show", "");
+        x.classList.remove('w3-show');
+        x.className.trimEnd()
+        console.log(x.className);
+        console.log("false");
+        return;
+    }
+    */
+
+}
